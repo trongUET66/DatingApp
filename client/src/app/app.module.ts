bc7fde5fb8f9e59d23e9a6b86770101861c4477a
trongUET66 <trong.uet.vnu@gmail.com>
2023-06-22 17:26:32 +0700

    Error chap 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,12 +14,13 @@ import { NavComponent } from './nav/nav.component';
     AppComponent,
     NavComponent
   ],
-  imports: [
+  imports:[
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
-  ],
+    BrowserAnimationsModule,
+    FormsModule
+  ],    
   providers: [],
   bootstrap: [AppComponent]
 })
